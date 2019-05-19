3906d2c90c2cca1f5c8c9c56a5880fcd6d9e6d9e
Jakub Kadlčík <frostyx@email.cz>
2019-05-19 13:24:43 +0200

    Use VimwikiFileProvider to read a file content

--- tests/test_vwunfinished.py
+++ tests/test_vwunfinished.py
@@ -1,13 +1,14 @@
+import six.moves
 import datetime
 import unittest
-from mock import patch, Mock
+from mock import patch, Mock, mock_open
 
 # Enable importing modules from parent directory
 import os
 parentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 os.sys.path.insert(0, parentdir)
 
-from vwunfinished import UnfinishedTasksCounter, parser
+from vwunfinished import UnfinishedTasksCounter, VimwikiFileProvider, parser
 
 
 class TestVWUnfinished(unittest.TestCase):
@@ -85,6 +86,35 @@ simple_text_vimwiki_syntax = """= 2019-05-18 =
 
 * [ ] Finish vimwiki article
 """
+
+
+class TestVimwikiFileProvider(unittest.TestCase):
+
+    @patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/home/mockee"))
+    def test_path(self, *args):
+        provider = VimwikiFileProvider(path="~/foo.md")
+        assert provider.path == "/home/mockee/foo.md"
+
+        provider = VimwikiFileProvider(date="2019-05-19", filetype="md")
+        assert provider.path == "/home/mockee/vimwiki/diary/2019-05-19.md"
+
+        provider = VimwikiFileProvider(date="2019-05-19", wiki_path="/foo/", diary_dir="bar", filetype="wiki")
+        assert provider.path == "/foo/bar/2019-05-19.wiki"
+
+    def test_path_precedence(self):
+        provider = VimwikiFileProvider(path="foo.md", date="2019-05-19")
+        assert provider.path == "foo.md"
+
+    def test_unspecified_path_exception(self):
+        with self.assertRaises(ValueError) as context:
+            provider = VimwikiFileProvider()
+            provider.path
+        assert "not enough information " in str(context.exception)
+
+    @patch("{}.open".format(six.moves.builtins.__name__), mock_open(read_data=simple_text))
+    def test_content(self):
+        provider = VimwikiFileProvider(path="whatever-we-mock-it.md")
+        assert "## Daily checklist" in provider.content
 
 
 class TestArgparser(unittest.TestCase):
--- vwunfinished.py
+++ vwunfinished.py
@@ -11,6 +11,7 @@ parser.add_argument("--bullets", help="Bullet symbols (e.g. '*-')", type=list)
 parser.add_argument("--diary-dir", default="diary", help="Use nonstandard diary directory")
 parser.add_argument("--wiki-path", default="~/vimwiki", help="Use nonstandard wiki path",
                     type=lambda x: os.path.expanduser(x))
+parser.add_argument("--filetype", default="wiki", choices=["wiki", "md"], help="What filetype do you use?")
 
 input_parser = parser.add_mutually_exclusive_group(required=True)
 input_parser.add_argument("--path", help="Path to a vimwiki file")
@@ -20,8 +21,7 @@ input_parser.add_argument("--today", help="Use diary file for today", dest="date
 
 
 class UnfinishedTasksCounter(object):
-    def __init__(self, path=None, text=None, section=None, bullets=None, count_sublists=True):
-        self.path = path
+    def __init__(self, text=None, section=None, bullets=None, count_sublists=True):
         self.bullets = bullets or ["-", "*"]
         self.section = section
         self._text = text
@@ -54,6 +54,31 @@ class UnfinishedTasksCounter(object):
         return len(self.unfinished_tasks)
 
 
+class VimwikiFileProvider(object):
+    def __init__(self, path=None, date=None, wiki_path="~/vimwiki", diary_dir="diary", filetype="wiki"):
+        self._path = path
+        self.date = date
+        self.wiki_path = wiki_path
+        self.diary_dir = diary_dir
+        self.filetype = filetype
+
+    @property
+    def path(self):
+        if self._path:
+            return os.path.expanduser(self._path)
+
+        if self.date:
+            return os.path.join(*[os.path.expanduser(self.wiki_path), self.diary_dir,
+                                  ".".join([self.date, self.filetype])])
+
+        raise ValueError("I have not enough information to determine a file")
+
+    @property
+    def content(self):
+        with open(self.path, "r") as f:
+            return f.read()
+
+
 def vimwiki_unfinished_tasks():
     # API
     pass
@@ -63,7 +88,10 @@ def main():
     args = parser.parse_args()
     if not os.path.exists(args.path):
         sys.exit(11)
-    counter = UnfinishedTasksCounter(path=args.path, section=args.section, bullets=args.bullets,
+
+    provider = VimwikiFileProvider(path=args.path, date=args.date, wiki_path=args.wiki_path,
+                                   diary_dir=args.diary_dir, filetype=args.filetype)
+    counter = UnfinishedTasksCounter(text=provider.content, section=args.section, bullets=args.bullets,
                                      count_sublists=args.count_sublists)
     print(counter.count_unfinished_tasks())
 
