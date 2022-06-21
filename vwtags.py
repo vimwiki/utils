96a03412c4140e22de912bfaa062b7f5aba86f0a
João Porto <78543243+joaoandreporto@users.noreply.github.com>
2022-06-21 12:20:17 +0000

    🛑 fenced comments clashing with markdown headers
    This proposes an initial solution to https://github.com/vimwiki/utils/issues/11 .

--- vwtags.py
+++ vwtags.py
@@ -1,9 +1,16 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+source: https://github.com/vimwiki/utils/blob/master/vwtags.py
+forked from the script by EinfachToll originally committed on 24 Jun 2014 to
+https://github.com/vimwiki/utils
+"""
 
 from __future__ import print_function
+import sys
+import re
 
-help_text = """
+help_text = r"""
 Extracts tags from Vimwiki files. Useful for the Tagbar plugin.
 
 Usage:
@@ -11,14 +18,14 @@ Install Tagbar (https://github.com/preservim/tagbar/). Then, put this file
 anywhere and add the following to your .vimrc:
 
 let g:tagbar_type_vimwiki = {
-          \   'ctagstype':'vimwiki'
-          \ , 'kinds':['h:header']
-          \ , 'sro':'&&&'
-          \ , 'kind2scope':{'h':'header'}
-          \ , 'sort':0
-          \ , 'ctagsbin':'/path/to/vwtags.py'
-          \ , 'ctagsargs': 'default'
-          \ }
+                        \   'ctagstype':'vimwiki'
+                        \ , 'kinds':['h:header']
+                        \ , 'sro':'&&&'
+                        \ , 'kind2scope':{'h':'header'}
+                        \ , 'sort':0
+                        \ , 'ctagsbin':'/path/to/vwtags.py'
+                        \ , 'ctagsargs': 'default'
+                        \ }
 
 The value of ctagsargs must be one of 'default', 'markdown' or 'media',
 whatever syntax you use. However, if you use multiple wikis with different
@@ -27,8 +34,14 @@ will show markdown style headers as well as default/mediawiki style headers,
 but there might be erroneously shown headers.
 """
 
-import sys
-import re
+
+class Error(Exception):
+    """Base class for exceptions."""
+
+
+class ReadFileIntoBufferError(Error):
+    """Exception raising for failed reading file into Buffer attempt"""
+
 
 if len(sys.argv) < 3:
     print(help_text)
@@ -38,6 +51,8 @@ syntax = sys.argv[1]
 filename = sys.argv[2]
 rx_default_media = r"^\s*(={1,6})([^=].*[^=])\1\s*$"
 rx_markdown = r"^\s*(#{1,6})([^#].*)$"
+rx_fenced_code = r"^```[^\r\n]*[a-z]*(?:\n(?!```$).*)*\n```"
+rx_header = None
 
 if syntax in ("default", "media"):
     rx_header = re.compile(rx_default_media)
@@ -46,37 +61,62 @@ elif syntax == "markdown":
 else:
     rx_header = re.compile(rx_default_media + "|" + rx_markdown)
 
-file_content = []
 try:
-    with open(filename, "r") as vim_buffer:
-        file_content = vim_buffer.readlines()
-except:
+    with open(filename, 'r') as buffer:
+        file_content = buffer.readlines()
+except ReadFileIntoBufferError:
+    print("Failed to open file")
     exit()
 
+
+def fenced_code_sentinel(filename):
+    """detect fenced code zones"""
+    # fenced code toggle init
+    fct = 0
+
+    # fct gen
+    for line in file_content:
+        if syntax == "markdown":
+            fct_d = fct
+
+            if fct_d == fct and re.match(r"```", line):
+                fct = 1 - fct_d
+            if re.match(r"```", line):
+                yield 1
+            else:
+                yield fct_d
+        else:
+            yield fct
+
+
+fcd = fenced_code_sentinel(filename)
 state = [""]*6
+
 for lnum, line in enumerate(file_content):
+    fcd_toggle = next(fcd)
 
-    match_header = rx_header.match(line)
+    if fcd_toggle == 0:
+        match_header = rx_header.match(line)
 
-    if not match_header:
-        continue
+        if not match_header:
+            continue
 
-    match_lvl = match_header.group(1) or match_header.group(3)
-    match_tag = match_header.group(2) or match_header.group(4)
+        match_lvl = match_header.group(1) or match_header.group(3)
+        match_tag = match_header.group(2) or match_header.group(4)
 
-    cur_lvl = len(match_lvl)
-    cur_tag = match_tag.strip()
-    cur_searchterm = "^" + match_header.group(0).rstrip("\r\n") + "$"
-    cur_kind = "h"
+        cur_lvl = len(match_lvl)
+        cur_tag = match_tag.strip()
+        cur_searchterm = "^" + match_header.group(0).rstrip("\r\n") + "$"
+        cur_kind = "h"
 
-    state[cur_lvl-1] = cur_tag
-    for i in range(cur_lvl, 6):
-        state[i] = ""
+        state[cur_lvl-1] = cur_tag
+        for i in range(cur_lvl, 6):
+            state[i] = ""
 
-    scope = "&&&".join(
-            [state[i] for i in range(0, cur_lvl-1) if state[i] != ""])
-    if scope:
-        scope = "\theader:" + scope
+        scope = "&&&".join(
+                [state[i] for i in range(0, cur_lvl-1) if state[i] != ""])
+        if scope:
+            scope = "\theader:" + scope
 
-    print('{0}\t{1}\t/{2}/;"\t{3}\tline:{4}{5}'.format(
-        cur_tag, filename, cur_searchterm, cur_kind, str(lnum+1), scope))
+        print('{0}\t{1}\t/{2}/;"\t{3}\tline:{4}{5}'.format(
+            cur_tag, filename, cur_searchterm, cur_kind, str(lnum+1), scope))
